fabc584ee005073988ab14f7829f8ca90a140564
omerolkun <omer.olkun@ug.bilkent.edu.tr>
2022-05-05 23:13:03 +0300

    code runs properly

--- scat_gat.c
+++ scat_gat.c
@@ -8,28 +8,47 @@ int main(int argc, char* argv []){
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    int** ptr;
-    int n = 8; //elements of array is n * n
-    int columns = n;
-    int rows = n;
+    
+    int n = size;
+    int arr[n][n];
     int root = 0;
-    int send_data[];
-    if(rank == root){
-        //allocating the array
-        ptr = (int**)malloc(rows*sizeof(int*));
-        for(int i = 0; i < rows; i++){
-            *(ptr + i ) = (int*)malloc(columns*sizeof(int));
-        }
+    int gathered_data_to_root [size];
+    if (rank  == root){
         srand(time(NULL));
-        //assign values to array
-        for(int i = 0; i < rows; i++){
-            for(int j=0; j < columns; j++){
-                ptr[i][j] = rand() % 50;
-                printf("%d ",ptr[i][j]);
+        for(int i = 0; i < n; i++){
+            for (int j = 0; j < n ; j++){
+                arr[i][j]  = rand() % 30;
+                printf("%d ",arr[i][j]);
             }
             printf("\n");
         }
+        /*for(int i = 0; i < size; i++){
+            printf("my rank is %d and gathered_data[%d] is %d \n",rank,i,gathered_data_to_root[i]);
+        } */       
     }
 
+    int recv[n];
+    MPI_Scatter(*arr,n,MPI_INT,recv,n,MPI_INT,root,MPI_COMM_WORLD);
+    for(int i = 0; i < n ; i++){
+        //printf("my rank is %d and recv[%d] is %d \n",rank,i,recv[i]);
+    }
+    int partial_sum = 0;
+    for (int i = 0; i < n; i++){
+        partial_sum = partial_sum + recv[i];
+    }
+    printf("rank %d and sum %d \n",rank, partial_sum);
+    MPI_Gather(&partial_sum,1,MPI_INT,&gathered_data_to_root,1,MPI_INT,root,MPI_COMM_WORLD);
+
+    if(rank == root){
+        int sum_of_arr = 0;
+        for(int i = 0; i < size; i++){
+            sum_of_arr = sum_of_arr + gathered_data_to_root[i];
+        }
+        printf("the sum of the array is %d \n",sum_of_arr);
+    }
+
+
     MPI_Finalize();
+
+    return 0;
 }
